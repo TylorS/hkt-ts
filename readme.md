5dd94193bbbe8f0f7ae91d5d122358ef4f140450
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-07-22 10:40:49 -0400

    feat: start to build out some stuff

--- readme.md
+++ readme.md
@@ -1,1 +1,61 @@
-# TODO
+# HKT-TS
+
+`HKT-TS` is an encoding for Higher-Kinded-Types (HKT) in TypeScript. 
+It requires writing 0 overloads for your functions, and does not need to modify an existing package.
+This means the use of HKTs for your libraries can be opt-in, delivered by an external package, and 
+potentially from the community!
+
+## Installation (NOT YET !!)
+
+```sh
+# NPM
+npm i --save hkt-ts
+
+# Yarn
+yarn install hkt-ts
+```
+
+## How to register a new HKT
+
+```typescript
+import { TypeParams } from 'hkt-ts'
+
+export const URI = Symbol.for('Either') // Could be string | number | symbol
+export type Left<A> = { left: true, value: A }
+export type Right<A> = { left: false, value: A}
+export type Either<A, B> = Left<A> | Right<B>
+
+// The URI must match in both type-level maps to work correctly.
+declare module "hkt-ts" {
+  // Setup how to pass new type-parameters to a Type, looked up by the given URI.
+  export interface Hkts<Values> {
+    // TypeParams.First, TypeParams.Second,..., TypeParams.Fifth, are helpers for extracting
+    // type parameters from Values working from right-to-left.
+    [URI]: Either<TypeParams.Second<Values>, TypeParams.First<Values>>
+  }
+  
+  // Setup how to extract a Tuple of type-parameters from a registered Type, looked up by the given URI.
+  export interface HktValues<T> {
+    // Wrapping in a tuple helps to avoid returning an invalid union e.g. [A, unknown] | [unknown, B]
+    [URI]: [T] extends [Either<infer A, infer B>] ? [A, B] : never
+  }
+}
+
+```
+
+## How to create a new type-class
+
+```typescript
+import { PossibleValues, Type, Types, TypeParams } from 'hkt-ts'
+
+// All type-classes should use an a `extends Types` clause to ensure it's working with
+// registered types.
+export interface Functor<T extends Types> {
+  // Adding the Type to any methods type-parameters will help with inference
+  // PossibleValues is a helpful placeholder for supporting Types with up to 5 type-parameters.
+  map: <A, B, F extends Type<T, [...PossibleValues, A]>>(f: (a: A) => b, functor: F) => 
+    Type<T, [...TypeParams.DropLast<F, 1>, B]> 
+    // TypeParams.DropLast (and TypeParams.Drop!) is another helper for creating type-classes. It retrieves the type-paramaters
+    // for a registered Type, and pops off up to 5 of the last values from the tuple.
+}
+```
--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,109 @@
-// TODO: write great things
+import { L, N, U } from 'ts-toolbelt'
+
+/**
+ * Type-level map, used to apply values to specific data structures. User's should
+ * extend this interface to register their custom types.
+ * @example
+ */
+// @ts-ignore - Allow compiling with unused type-param
+export interface Hkts<Values extends ReadonlyArray<any>> {}
+
+/**
+ * Type-level map, used to extract values from instances to apply transformations or just
+ * to retrieve the information. User's should extend this interface to register their custom types.
+ */
+// @ts-ignore - Allow compiling with unused type-param
+export interface HktValues<T> extends Record<PropertyKey, PossibleValues> {}
+
+/**
+ * Union of all type names as defined in Hkts & HktValues
+ */
+export type Types = keyof Hkts<ReadonlyArray<any>> & keyof HktValues<any>
+
+/**
+ * Helper for type-classes to use a placeholder type-params
+ */
+export type PossibleValues = [any?, any?, any?, any?, any?]
+
+/**
+ * Helper for creating types by use of their Type name, and a tuple of values to use.
+ *
+ * @example
+ * Type<'Either', [Error, number]> === Either<Error, number>
+ */
+export type Type<
+  T extends Types = Types,
+  Values extends ReadonlyArray<any> = ReadonlyArray<any>
+> = Hkts<Values>[T]
+
+/**
+ * Lookup the name of Hkt by Type. In the event of there being multiple matches
+ * because they are structurally equivalent, whichever type is registered last will be chosen.
+ * @example
+ * TypeToName<Either<any, any>> === 'Either'
+ */
+export type TypeToName<A> = CastToTypeName<
+  U.Last<
+    {
+      [T in Types]: Type<T, ReadonlyArray<any>> extends A ? T : never
+    }[Types]
+  >
+>
+
+type CastToTypeName<A> = A extends Types ? A : never
+
+/**
+ * Helpers for working with Type Parameters
+ */
+export namespace TypeParams {
+  /**
+   * Extract the first type-param from Values of Hkts
+   */
+  export type First<A> = A extends L.List ? L.Last<A> : never
+  /**
+   * Extract the second type-param from Values of Hkts
+   */
+  export type Second<A> = A extends L.List ? L.Last<L.Pop<A>> : never
+  /**
+   * Extract the third type-param from Values of Hkts
+   */
+  export type Third<A> = A extends L.List ? L.Last<L.Pop<L.Pop<A>>> : never
+  /**
+   * Extract the fourth type-param from Values of Hkts
+   */
+  export type Fourth<A> = A extends L.List ? L.Last<L.Pop<L.Pop<L.Pop<A>>>> : never
+  /**
+   * Extract the fifth type-param from Values of Hkts
+   */
+  export type Fifth<A> = A extends L.List ? L.Last<L.Pop<L.Pop<L.Pop<L.Pop<A>>>>> : never
+
+  /**
+   * Retrieve the type-parameters of an Hkt as a Tuple.
+   * @example
+   * TypeParams.Of<Either<A, B>> === [A, B]
+   */
+  export type Of<A extends Type<Types, PossibleValues>> = CastArray<HktValues<A>[TypeToName<A>]>
+
+  type CastArray<A> = A extends ReadonlyArray<any> ? A : []
+
+  /**
+   * Retrieve the type-parameters of a given Type while dropping off a given amount from the start.
+   * Useful for creating type-classes.
+   */
+  export type Drop<A extends Type<Types, PossibleValues>, N extends 1 | 2 | 3 | 4 | 5> = L.Drop<
+    Of<A>,
+    N.NumberOf<N>
+  >
+
+  /**
+   * Retrieve the type-parameters of a given Type while dropping off a given amount from the end.
+   * Useful for creating type-classes.
+   */
+  export type DropLast<A extends Type<Types, PossibleValues>, N extends 1 | 2 | 3 | 4 | 5> = {
+    1: L.Pop<Of<A>>
+    2: L.Pop<L.Pop<Of<A>>>
+    3: L.Pop<L.Pop<L.Pop<Of<A>>>>
+    4: L.Pop<L.Pop<L.Pop<L.Pop<Of<A>>>>>
+    5: L.Pop<L.Pop<L.Pop<L.Pop<L.Pop<Of<A>>>>>>
+  }[N extends 1 | 2 | 3 | 4 | 5 ? N : never]
+}
