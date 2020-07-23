1665423aeb72d48c8de33e12130443cea918150f
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-07-23 12:31:02 -0400

    docs: add docs for Ord

--- source/type-classes/Ord.ts
+++ source/type-classes/Ord.ts
@@ -1,6 +1,13 @@
 import { PossibleValues, Type, Types } from '../Hkts'
 import { Setoid } from './Setoid'
 
+/**
+ * @name Ord
+ * @laws
+ * Totality: Ord.lte(a, b) or Ord.lte(b, a)
+ * Antisymmetry: if Ord.lte(a, b) and Ord.lte(b, a), then Ord.equals(a, b)
+ * Transitivity: if Ord.lte(a, b) and Ord.lte(b, c), then Ord.lte(a, c)
+ */
 export interface Ord<T extends Types> extends Setoid<T> {
   readonly lte: <A extends Type<T, PossibleValues>>(a: A, b: A) => boolean
 }
