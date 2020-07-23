728c3bbd0f1003337970a80400a955f7f4cc738f
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-07-23 12:29:39 -0400

    docs: add some docs for Setoid

--- source/type-classes/Setoid.ts
+++ source/type-classes/Setoid.ts
@@ -1,5 +1,12 @@
 import { PossibleValues, Type, Types } from '../Hkts'
 
+/**
+ * @name Setoid
+ * @laws
+ * Reflexivity: Setoid.equals(a, a) === true
+ * Symmetry: Setoid.equals(a, b) === Setoid.equals(b, a)
+ * Transitivity: if Setoid.equals(a, b) and Setoid.equals(b, c), then Setoid.equals(a, c)
+ */
 export interface Setoid<T extends Types> {
   readonly equals: <A extends Type<T, PossibleValues>>(a: A, b: A) => boolean
 }
