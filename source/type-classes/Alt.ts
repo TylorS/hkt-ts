681a5edbd4238c262e503f75db90367f701db7b4
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-07-23 13:06:26 -0400

    fix: align Alt with static-land

--- source/type-classes/Alt.ts
+++ source/type-classes/Alt.ts
@@ -1,8 +1,15 @@
 import { PossibleValues, Type, Types } from '../Hkts'
+import { Functor } from './Functor'
 
-export interface Alt<T extends Types> {
+/**
+ * @name Alt
+ * @laws
+ * Associativity: A.alt(A.alt(a, b), c) ≡ A.alt(a, A.alt(b, c))
+ * Distributivity: A.map(f, A.alt(a, b)) ≡ A.alt(A.map(f, a), A.map(f, b))
+ */
+export interface Alt<T extends Types> extends Functor<T> {
   readonly alt: <A extends Type<T, PossibleValues>, B extends Type<T, PossibleValues>>(
-    f: () => A,
-    functor: B,
+    a: A,
+    b: B,
   ) => A | B
 }
