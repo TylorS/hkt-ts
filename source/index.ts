5552ef62d456a853cae6bb285c75979ff5c82e67
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-07-23 11:58:24 -0400

    feat: simplify Drop + TypeParams helpers

--- source/index.ts
+++ source/index.ts
@@ -57,40 +57,40 @@ type CastToTypeName<A> = A extends Types ? A : never
  */
 export namespace TypeParams {
   /**
-   * Extract the first type-param from Values of Hkts
-   */
-  export type First<A> = A extends L.List ? L.Last<A> : never
-  /**
-   * Extract the second type-param from Values of Hkts
-   */
-  export type Second<A> = A extends L.List ? L.Last<L.Pop<A>> : never
-  /**
-   * Extract the third type-param from Values of Hkts
-   */
-  export type Third<A> = A extends L.List ? L.Last<L.Pop<L.Pop<A>>> : never
-  /**
-   * Extract the fourth type-param from Values of Hkts
-   */
-  export type Fourth<A> = A extends L.List ? L.Last<L.Pop<L.Pop<L.Pop<A>>>> : never
-  /**
-   * Extract the fifth type-param from Values of Hkts
-   */
-  export type Fifth<A> = A extends L.List ? L.Last<L.Pop<L.Pop<L.Pop<L.Pop<A>>>>> : never
-
-  /**
    * Retrieve the type-parameters of an Hkt as a Tuple.
    * @example
    * TypeParams.Of<Either<A, B>> === [A, B]
    */
   export type Of<A extends Type<Types, PossibleValues>> = CastArray<HktValues<A>[TypeToName<A>]>
 
-  type CastArray<A> = A extends ReadonlyArray<any> ? A : []
+  type CastArray<A> = A extends L.List ? A : []
+
+  /**
+   * Extract the first type-param from Values of Hkts
+   */
+  export type First<A> = A extends L.List ? L.Last<A> : never
+  /**
+   * Extract the second type-param from Values of Hkts
+   */
+  export type Second<A> = A extends L.List ? L.Last<L.Drop<A, '1', '<-'>> : never
+  /**
+   * Extract the third type-param from Values of Hkts
+   */
+  export type Third<A> = A extends L.List ? L.Last<L.Drop<A, '2', '<-'>> : never
+  /**
+   * Extract the fourth type-param from Values of Hkts
+   */
+  export type Fourth<A> = A extends L.List ? L.Last<L.Drop<A, '3', '<-'>> : never
+  /**
+   * Extract the fifth type-param from Values of Hkts
+   */
+  export type Fifth<A> = A extends L.List ? L.Last<L.Drop<A, '4', '<-'>> : never
 
   /**
    * Retrieve the type-parameters of a given Type while dropping off a given amount from the start.
    * Useful for creating type-classes.
    */
-  export type Drop<A extends Type<Types, PossibleValues>, N extends 1 | 2 | 3 | 4 | 5> = L.Drop<
+  export type Drop<A extends Type<Types, PossibleValues>, N extends 0 | 1 | 2 | 3 | 4 | 5> = L.Drop<
     Of<A>,
     N.NumberOf<N>
   >
