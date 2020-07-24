4136851bbca23116da7b3a74e3072d2d70900019
Tylor Steinberger <tlsteinberger167@gmail.com>
2020-07-23 23:45:30 -0400

    fix: ChainRec type-signature
    Adds missing value to 'f' callback

--- source/type-classes/ChainRec.ts
+++ source/type-classes/ChainRec.ts
@@ -4,23 +4,39 @@ import { TypeParams } from '../TypeParams'
 export interface ChainRec<T extends Types> {
   readonly chainRec: {
     1: <A, B>(
-      f: (n: (a: A) => Next<A>, d: (b: B) => Done<B>) => Type<T, [Next<A> | Done<B>]>,
+      f: (next: (a: A) => Next<A>, done: (b: B) => Done<B>, a: A) => Type<T, [Next<A> | Done<B>]>,
       a: A,
     ) => Type<T, [B]>
     2: <A, B, C>(
-      f: (n: (a: A) => Next<A>, d: (b: B) => Done<B>) => Type<T, [C, Next<A> | Done<B>]>,
+      f: (
+        next: (a: A) => Next<A>,
+        done: (b: B) => Done<B>,
+        a: A,
+      ) => Type<T, [C, Next<A> | Done<B>]>,
       a: A,
     ) => Type<T, [C, B]>
     3: <A, B, C, D>(
-      f: (n: (a: A) => Next<A>, d: (b: B) => Done<B>) => Type<T, [C, D, Next<A> | Done<B>]>,
+      f: (
+        next: (a: A) => Next<A>,
+        done: (b: B) => Done<B>,
+        a: A,
+      ) => Type<T, [C, D, Next<A> | Done<B>]>,
       a: A,
     ) => Type<T, [C, D, B]>
     4: <A, B, C, D, E>(
-      f: (n: (a: A) => Next<A>, d: (b: B) => Done<B>) => Type<T, [C, D, E, Next<A> | Done<B>]>,
+      f: (
+        next: (a: A) => Next<A>,
+        done: (b: B) => Done<B>,
+        a: A,
+      ) => Type<T, [C, D, E, Next<A> | Done<B>]>,
       a: A,
     ) => Type<T, [C, D, E, B]>
     5: <A, B, C, D, E, F>(
-      f: (n: (a: A) => Next<A>, d: (b: B) => Done<B>) => Type<T, [C, D, E, F, Next<A> | Done<B>]>,
+      f: (
+        next: (a: A) => Next<A>,
+        done: (b: B) => Done<B>,
+        a: A,
+      ) => Type<T, [C, D, E, F, Next<A> | Done<B>]>,
       a: A,
     ) => Type<T, [C, D, E, F, B]>
   }[TypeParams.Length<T>]
