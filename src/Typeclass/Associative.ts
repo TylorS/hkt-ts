f26686d9b5651e076da21cae0d4ff89997ed8ed5
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-05-14 13:51:19 -0400

    docs: Associative docs

--- src/Typeclass/Associative.ts
+++ src/Typeclass/Associative.ts
@@ -1,14 +1,29 @@
-import * as ASSOC from './Concat'
+import * as C from './Concat'
 
-// Associative represents a concatenation that MUST be associatve.
-export type Associative<A> = ASSOC.Concat<A>
+/**
+ * Associative is a typeclass for conrete values that support a concatenation that is associative.
+ *
+ * Laws:
+ * Associativity :: concat(concat(a, b), c) === concat(a, concat(b, c))
+ */
+export type Associative<A> = C.Concat<A>
 
+/**
+ * Create an Associative instance that always return the same value.
+ */
 export const constant = <A>(value: A): Associative<A> => ({
   concat: () => value,
 })
 
-export const reverse: <A>(S: Associative<A>) => Associative<A> = ASSOC.reverse
+/**
+ * Change the ordering of an Associative concatenation
+ */
+export const reverse: <A>(A: Associative<A>) => Associative<A> = C.reverse
 
+/**
+ * Construct an Associative for an object defined in terms of Associative's
+ * for all of the properties.
+ */
 export const struct = <A>(associatives: {
   readonly [K in keyof A]: Associative<A[K]>
 }): Associative<{
@@ -23,21 +38,38 @@ export const struct = <A>(associatives: {
   },
 })
 
+/**
+ * Construct an Associative for a tuple defined in terms of Associative's
+ * for all each index.
+ */
 export const tuple = <A extends ReadonlyArray<unknown>>(
   ...associatives: { readonly [K in keyof A]: Associative<A[K]> }
 ): Associative<Readonly<A>> => ({
   concat: (first, second) => associatives.map((s, i) => s.concat(first[i], second[i])) as any,
 })
 
+/**
+ * Combinator to zip a value in between the other values during concatenation.
+ * Similar to ['a', 'b'].join(', ') but for all types instead of just strings.
+ */
 export const intercalate =
   <A>(middle: A) =>
   (A: Associative<A>): Associative<A> => ({
     concat: (x, y) => A.concat(x, A.concat(middle, y)),
   })
 
-export const concatAll: <A>(M: Associative<A>) => (startWith: A) => (as: readonly A[]) => A =
-  ASSOC.concatAll
+/**
+ * Concatenate an array of values given a seed.
+ */
+export const concatAll: <A>(A: Associative<A>) => (startWith: A) => (as: readonly A[]) => A =
+  C.concatAll
 
-export const First: Associative<any> = ASSOC.First
+/**
+ * An Assocative instance which will always return the first value.
+ */
+export const First: Associative<any> = C.First
 
-export const Second: Associative<any> = ASSOC.Second
+/**
+ * An Assocative instance which will always return the second value.
+ */
+export const Second: Associative<any> = C.Second
