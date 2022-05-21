6e1f39d284d74d365c2e5628a0df166ac9d4d7ba
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-05-21 14:35:22 -0400

    refactor: make a single statement

--- src/Typeclass/Eq.test.ts
+++ src/Typeclass/Eq.test.ts
@@ -2,23 +2,25 @@ import * as fc from 'fast-check'
 
 import * as L from '../Law/index'
 import * as B from '../boolean'
+import { pipe } from '../function'
 import * as N from '../number'
 
 import * as E from './Eq'
 
 describe(__filename, () => {
   describe('Contravariant', () => {
-    const test = L.Contravariant.testContravariant(
-      E.Contravariant,
-      (a: number) => String(a),
-      (s: string) => s.length % 2 === 0,
-      N.Eq,
-      B.Eq,
-      <A>(eq: E.Eq<A>, a: A, b: A) => eq.equals(a, b),
-      B.Eq,
-    )
-
-    const { identity, associativity } = test(L.number())(fc)
+    const { identity, associativity } = pipe(
+      L.number(),
+      L.Contravariant.testContravariant(
+        E.Contravariant,
+        (a: number) => String(a),
+        (s: string) => s.length % 2 === 0,
+        N.Eq,
+        B.Eq,
+        <A>(eq: E.Eq<A>, a: A, b: A) => eq.equals(a, b),
+        B.Eq,
+      ),
+    )(fc)
 
     it('Identity', identity)
     it('Associativity', associativity)
