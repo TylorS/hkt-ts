2dc858d8a9ce79bb00b65fa77cb5392940584984
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-05-26 19:35:22 -0400

    test: Set instances

--- src/Set.ts
+++ src/Set.ts
@@ -1,18 +1,28 @@
 import * as A from './Array'
 import { Left, Right } from './Either'
-import { HKT, Params } from './HKT'
+import { HKT, Kind, Params } from './HKT'
+import * as M from './Maybe'
+import { Associative } from './Typeclass/Associative'
 import * as AE from './Typeclass/AssociativeEither'
 import * as AF from './Typeclass/AssociativeFlatten'
 import { Bottom1 } from './Typeclass/Bottom'
+import { Compact1 } from './Typeclass/Compact'
 import { Concat } from './Typeclass/Concat'
 import * as C from './Typeclass/Covariant'
 import { Debug } from './Typeclass/Debug'
 import * as Eq from './Typeclass/Eq'
+import * as FIM from './Typeclass/FilterMap'
+import * as FM from './Typeclass/FoldMap'
+import * as FE from './Typeclass/ForEach/index'
+import { fromIdentityEitherCovariant } from './Typeclass/Identity'
 import * as IB from './Typeclass/IdentityBoth'
 import { IdentityEither1 } from './Typeclass/IdentityEither'
+import { IdentityFlatten1 } from './Typeclass/IdentityFlatten'
 import * as Ord from './Typeclass/Ord'
+import * as PM from './Typeclass/PartitionMap'
+import { Separate1 } from './Typeclass/Separate'
 import * as T from './Typeclass/Top'
-import { pipe } from './function'
+import { flow, pipe } from './function'
 
 export interface SetHKT extends HKT {
   readonly type: ReadonlySet<this[Params.A]>
@@ -73,6 +83,11 @@ export const IdentityBoth: IB.IdentityBoth1<SetHKT> = {
 export const tuple = IB.tuple<SetHKT>({ ...IdentityBoth, ...Covariant })
 export const struct = IB.struct<SetHKT>({ ...IdentityBoth, ...Covariant })
 
+export const IdentityFlatten: IdentityFlatten1<SetHKT> = {
+  ...Flatten,
+  ...Top,
+}
+
 export const AssociativeEither: AE.AssociativeEither1<SetHKT> = {
   either: (second) => (first) =>
     first.size === 0 ? pipe(second, map(Right)) : pipe(first, map(Left)),
@@ -111,3 +126,82 @@ export const makeOrd = <A>(O: Ord.Ord<A>): Ord.Ord<ReadonlySet<A>> =>
 export const makeDebug = <A>(D: Debug<A>): Debug<ReadonlySet<A>> => ({
   debug: (set) => `Set ${A.makeDebug(D).debug(Array.from(set))}`,
 })
+
+export const makeAssociative = <A>(): Associative<ReadonlySet<A>> => ({
+  concat: (f, s) => new Set([...f, ...s]),
+})
+
+export const makeIdentity = fromIdentityEitherCovariant<SetHKT>({ ...IdentityEither, ...Covariant })
+
+export const ForEach: FE.ForEach1<SetHKT> = {
+  map,
+  forEach: <T2 extends HKT>(IBC: IB.IdentityBoth<T2> & C.Covariant<T2>) => {
+    const tuple = IB.tuple(IBC)
+
+    return <A, B>(f: (a: A) => Kind<T2, B>) =>
+      (kind: ReadonlySet<A>): Kind<T2, ReadonlySet<B>> =>
+        tuple(...Array.from(kind).map(f))
+  },
+}
+
+export const forEach = ForEach.forEach
+export const sequence = FE.sequence(ForEach)
+export const mapAccum = FE.mapAccum(ForEach)
+export const foldMap = FE.foldMap(ForEach)
+
+export const FoldMap: FM.FoldMap1<SetHKT> = {
+  foldMap,
+}
+
+export const foldLeft = FM.foldLeft(FoldMap)
+export const contains = FM.contains(FoldMap)
+export const count = FM.count(FoldMap)
+export const exists = FM.exists(FoldMap)
+export const find = FM.find(FoldMap)
+export const every = FM.every(FoldMap)
+export const some = FM.some(FoldMap)
+export const groupBy = FM.groupBy(FoldMap)
+export const intercalate = FM.intercalate(FoldMap)
+export const isEmpty = <A>(a: ReadonlySet<A>): boolean => a.size === 0
+export const isNonEmpty = <A>(a: ReadonlySet<A>): boolean => a.size > 0
+export const toArray = FM.toArray(FoldMap)
+export const partitionMap = FM.partitionMap<SetHKT>({
+  ...FoldMap,
+  ...IdentityEither,
+  ...Top,
+  ...Covariant,
+})
+
+export const PartitionMap: PM.PartitionMap1<SetHKT> = {
+  partitionMap,
+}
+
+export const separate = PM.separate(PartitionMap)
+
+export const Separate: Separate1<SetHKT> = {
+  separate,
+}
+
+export const FilterMap: FIM.FilterMap1<SetHKT> = {
+  filterMap: (f) => (set) =>
+    new Set(
+      [...set].flatMap(
+        flow(
+          f,
+          M.tupled,
+          M.getOrElse(() => []),
+        ),
+      ),
+    ),
+}
+
+export const filterMap = FilterMap.filterMap
+export const compact = FIM.compact(FilterMap)
+export const filter = FIM.filter(FilterMap)
+
+export const Compact: Compact1<SetHKT> = {
+  compact,
+}
+
+export const compacted = FE.compacted<SetHKT>({ ...ForEach, ...Compact })
+export const separated = FE.separated<SetHKT>({ ...ForEach, ...Separate })
