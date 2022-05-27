e0c05d324d90f24dec325d49bbadbb1f7079a585
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-05-26 20:50:26 -0400

    test: Inverse

--- src/Typeclass/Inverse.test.ts
+++ src/Typeclass/Inverse.test.ts
@@ -10,10 +10,18 @@ describe(__filename, () => {
     it('allows multiplying a value', () => {
       deepStrictEqual(multiply(Inverse)(2, 2), 4)
       deepStrictEqual(multiply(Inverse)(2, -2), -4)
-      deepStrictEqual(multiply(InverseAll)(true, 2), true)
-      deepStrictEqual(multiply(InverseAll)(false, 2), false)
-      deepStrictEqual(multiply(InverseAny)(true, 2), true)
-      deepStrictEqual(multiply(InverseAny)(false, 2), false)
+
+      deepStrictEqual(multiply(InverseAll)(true, 2), true, 'InverseAll :: true * 2 = true')
+      deepStrictEqual(multiply(InverseAll)(true, -1), false, 'InverseAll :: true * -1 = false')
+      deepStrictEqual(multiply(InverseAll)(false, 2), false, 'InverseAll :: false * 2 = false')
+      deepStrictEqual(multiply(InverseAll)(false, 1), false, 'InverseAll :: false * 1 = false')
+      deepStrictEqual(multiply(InverseAll)(false, -1), true, 'InverseAll :: false * -1 = true')
+
+      deepStrictEqual(multiply(InverseAny)(true, 2), true, 'InverseAny :: true * 2 = true')
+      deepStrictEqual(multiply(InverseAny)(true, -1), false, 'InverseAny :: true * -1 = false')
+      deepStrictEqual(multiply(InverseAny)(false, 2), false, 'InverseAny :: false * 2 = false')
+      deepStrictEqual(multiply(InverseAny)(false, 1), false, 'InverseAny :: false * 1 = false')
+      deepStrictEqual(multiply(InverseAny)(false, -1), true, 'InverseAny :: false * -1 = true')
     })
   })
 })
